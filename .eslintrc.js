0885b57a02ef439ffaccb3446d20690d0ab19475
rodoabad <rodoabad@gmail.com>
2017-12-21 06:25:08 -0600

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: 'rodoabad',
+    rules: {
+        'arrow-body-style': 0,
+        'padded-blocks': [
+            2,
+            'never'
+        ],
+        'object-curly-newline': 0,
+        'react/jsx-no-bind': 0
+    }
+};
